449c9685c74cc5b21af5a1961436ae7dc01e923f
FilipeVillar <filipevillar7@gmail.com>
2020-05-10 16:28:37 +0100

    Add Completed winChecker class
    We completed the class having the 2 win conditions

--- FelliGame/WinChecker.cs
+++ FelliGame/WinChecker.cs
@@ -6,27 +6,35 @@ namespace FelliGame
     {
         public State Check(Board board)
         {
-            if (CheckForWin(board, State.Black)) return State.White;
-            if (CheckForWin(board, State.White)) return State.Black;
+            if (CheckForLose(board, State.Black)) return State.White;
+            if (CheckForLose(board, State.White)) return State.Black;
             return State.Undecided;
         }
 
-        private bool CheckForWin(Board board, State player)
+        private bool CheckForLose(Board board, State player)
         {
+            int found = 0;
+            int cantMove = 0;
             for (int row = 0; row < 5; row++)
             {
                 for (int column = 0; column < 5; column++)
                 {
                     if (board.GetState(new Position(row, column)) == player)
                     {
-                        if (board.checkMove())
-                        {
-                            return true;
-                        }
-                        return false;
-                    }    
+                        found =+ 1;
+                        if (CheckForMove(board, player)) cantMove += 1;
+                    }
                 }
             }
+            if (found != cantMove) return false;
             return true;
         }
+
+        private bool CheckForMove(Board board, State player)
+        {
+            if (board.CanMove(player)) return false;
+
+            return true;
+        }
+    }
 }
