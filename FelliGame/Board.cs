ccc96418b6d72201461cf1435581175654479635
FPTheFluffyPawed <n.ito10@hotmail.com>
2020-05-11 17:40:29 +0100

    Add Movement and display possible moves
    Board will now demonstrate the possible movements of a piece, and only allow movement of the possible pieces. Not only that, but if its in the center of the board (the only place that allows diagonal movement), it will add 4 new movement options.

--- FelliGame/Board.cs
+++ FelliGame/Board.cs
@@ -20,9 +20,18 @@ namespace FelliGame
         // It will automatically assign the states to the proper locations.
         public Board()
         {
-            board = new Piece[5, 5];
+            board = new Piece[5, 3];
             NextTurn = State.Black;
             AssignStates();
+        }
+
+        private bool IsPieceInCenter(Position pos)
+        {
+            // Center is (2, 1).
+            if (pos.X == 2 && pos.Y == 1)
+                return true;
+            else
+                return false;
         }
 
         /// <summary>
@@ -42,25 +51,51 @@ namespace FelliGame
             // This is just so we can't hop over our own pieces to "eat",
             // because that makes no sense.
 
-            Position[] possiblePositions = new Position[8];
+            // If the piece is in the center, we'll add the diagonal movement
+            // to check as well.
+            int possiblePos;
 
-            // All possible movement positions go here.
+            if (IsPieceInCenter(currentPos))
+                possiblePos = 8;
+            else
+                possiblePos = 4;
+
+            // Create the possible positions.
+            Position[] possiblePositions = new Position[possiblePos];
+
+            // Add our possible positions.
+            // Upper Middle.
             possiblePositions[0] = new Position(
-                currentPos.X - 1, currentPos.Y - 1);
+                currentPos.X - 1, currentPos.Y);
+            // Middle Left.
             possiblePositions[1] = new Position(
                 currentPos.X, currentPos.Y - 1);
+            // Middle Right.
             possiblePositions[2] = new Position(
-                currentPos.X + 1, currentPos.Y - 1);
-            possiblePositions[3] = new Position(
-                currentPos.X - 1, currentPos.Y);
-            possiblePositions[4] = new Position(
-                currentPos.X + 1, currentPos.Y);
-            possiblePositions[5] = new Position(
-                currentPos.X - 1, currentPos.Y + 1);
-            possiblePositions[6] = new Position(
                 currentPos.X, currentPos.Y + 1);
-            possiblePositions[7] = new Position(
-                currentPos.X + 1, currentPos.Y + 1);
+            // Lower Middle.
+            possiblePositions[3] = new Position(
+                currentPos.X + 1, currentPos.Y);
+
+            // If the piece is in the center, add the diagonal options.
+            if (IsPieceInCenter(currentPos))
+            {
+                // Upper Left.
+                possiblePositions[4] = new Position(
+                    currentPos.X + 1, currentPos.Y + 1);
+
+                // Upper Right.
+                possiblePositions[5] = new Position(
+                    currentPos.X - 1, currentPos.Y + 1);
+
+                // Lower Left.
+                possiblePositions[6] = new Position(
+                    currentPos.X + 1, currentPos.Y - 1);
+
+                // Lower Right.
+                possiblePositions[7] = new Position(
+                    currentPos.X - 1, currentPos.Y - 1);
+            }
 
             for (int i = 0; i < possiblePositions.Length; i++)
                 if (!IsOccupied(possiblePositions[i]))
@@ -70,39 +105,84 @@ namespace FelliGame
 
         public Position Move(Piece selectedPiece)
         {
-            // The position to move towards...
-            Position moveTo;
-            // Auxiliary string for input.
-            string aux;
+            // The converted input.
+            int convertedAux = 0;
 
-            do
+            // Boolean to check if the output convertion worked..
+            bool convertSuccesful = false;
+
+            // Invalid position to say that it was a failed move.
+            Position blockedPosition = new Position(0, 2);
+
+            // Array of possible destinations.
+            Position[] destinations = new Position[8];
+
+            // Add our possible positions.
+            // Upper Middle.
+            destinations[0] = new Position(
+                selectedPiece.Pos.X - 1, selectedPiece.Pos.Y);
+            // Middle Left.
+            destinations[1] = new Position(
+                selectedPiece.Pos.X, selectedPiece.Pos.Y - 1);
+            // Middle Right.
+            destinations[2] = new Position(
+                selectedPiece.Pos.X, selectedPiece.Pos.Y + 1);
+            // Lower Middle.
+            destinations[3] = new Position(
+                selectedPiece.Pos.X + 1, selectedPiece.Pos.Y);
+
+            // If the piece is in the center, add the diagonal options.
+            if (IsPieceInCenter(selectedPiece.Pos))
             {
+                // Upper Left.
+                destinations[4] = new Position(
+                    selectedPiece.Pos.X + 1, selectedPiece.Pos.Y + 1);
 
-                Console.WriteLine("Which direction?");
-                Console.WriteLine("Up/Left (1) | Up/Middle (2) | Up/Right (3)");
-                Console.WriteLine("Middle/Left (4) | Middle/Right (5)");
-                Console.WriteLine("Lower/Left (6) | Lower/Middle (7) |" +
-                    " Lower/Right (8)");
-                aux = Console.ReadLine();
+                // Upper Right.
+                destinations[5] = new Position(
+                    selectedPiece.Pos.X - 1, selectedPiece.Pos.Y + 1);
 
-                switch(aux)
+                // Lower Left.
+                destinations[6] = new Position(
+                    selectedPiece.Pos.X + 1, selectedPiece.Pos.Y - 1);
+
+                // Lower Right.
+                destinations[7] = new Position(
+                    selectedPiece.Pos.X - 1, selectedPiece.Pos.Y - 1);
+            }
+
+            Console.WriteLine("Which direction?");
+
+            Console.WriteLine("Up/Middle (1) | Middle/Left (2) |" +
+                " Middle/Right (3) | Lower/Middle (4)");
+
+            // If our piece is in the middle, it can move diagonally.
+            if (IsPieceInCenter(selectedPiece.Pos))
+                Console.WriteLine("Up/Left (5) | Up/Right (6) |" +
+                    " Lower/Left (7) | Lower/Right (8)");
+
+            while(!convertSuccesful)
+            {
+                Console.Write("Which direction? (Insert a valid option.)");
+                convertSuccesful = int.TryParse(Console.ReadLine(),
+                    out convertedAux)
+                    && (convertedAux > 0 && convertedAux <= 8);
+            }
+
+            // Adjust for our ForLoop.
+            convertedAux--;
+
+            for(int i = 1; i <= destinations.Length; i++)
+            {
+                if(i == convertedAux)
                 {
-                    case "1":
-                        moveTo = new Position(selectedPiece.Pos.X - 1, selectedPiece.Pos.Y - 1);
-                        if(CanMoveToLocation(moveTo))
-                        {
-                            MovePiece(selectedPiece, moveTo);
-                            selectedPiece.Pos = moveTo;
-                            break;
-                        }
-                        aux = null;
-                        break;
+                    if(CanMoveToLocation(destinations[i]))
+                    {
+                        return destinations[i];
+                    }
                 }
-
-                // Check for available movement options,
-                // and only print out the possible ones.
-
-            } while (aux == null);
+            }
+            return blockedPosition;
         }
 
         private void MovePiece(Piece piece, Position position)
@@ -114,11 +194,13 @@ namespace FelliGame
             board[piece.Pos.X, piece.Pos.Y] = null;
         }
 
-        public bool SetState(Position position, State newState)
+        public bool WasTurnSuccesful(Piece piece, Position position)
         {
-            if (newState != NextTurn) return false;
+            // If this spot is occupied, return it as false.
             if (IsOccupied(position)) return false;
 
+            // Else, we move the position and then move onto next turn.
+            MovePiece(piece, position);
             SwitchNextTurn();
             return true;
         }
@@ -130,24 +212,23 @@ namespace FelliGame
             // This will run through the board and
             // check which ones can move. Printing
             // Out the relevant pieces
-            Console.WriteLine("\nPossible positions:");
+            Console.WriteLine("\nAvailable pieces for movement:");
             for (int x = 0; x < board.GetLength(0); x++)
                 for(int y = 0; y < board.GetLength(1); y++)
                 {
                     Position position = new Position(x, y);
+                    number++;
 
-                    if(IsOccupied(position))
+                    if (IsOccupied(position))
                     {
-                        if(GetPiece(position).State == State.Black)
+                        if(GetPiece(position).State == currentPlayer)
                         {
                             if (CanMoveAtAll(position))
                             {
-                                Console.WriteLine($"({number}) - {x + 1}, {y + 1}");
+                                Console.WriteLine($"({number}) - {x}, {y}");
                             }
                         }
                     }
-
-                    number++;
                 }
             Console.WriteLine();
         }
@@ -161,10 +242,9 @@ namespace FelliGame
         {
             // Check if the position is out of bounds of the board.
             if (pos.Y > board.GetLength(1) - 1 || pos.Y < 0
-                || pos.X > board.GetLength(0) || pos.X < 0)
+                || pos.X > board.GetLength(0) - 1 || pos.X < 0)
                 return true;
-            // Else if the position is simply null, meaning that its a free
-            // spot.
+            // Else check if the position is free.
             else
                 return board[pos.X, pos.Y] != null;
         }
@@ -203,29 +283,23 @@ namespace FelliGame
             // From top to down.
             // BLACK SIDE
             SetInitialLocation(0, 0, State.Black);
+            SetInitialLocation(0, 1, State.Black);
             SetInitialLocation(0, 2, State.Black);
-            SetInitialLocation(0, 4, State.Black);
+            SetInitialLocation(1, 0, State.Black);
             SetInitialLocation(1, 1, State.Black);
             SetInitialLocation(1, 2, State.Black);
-            SetInitialLocation(1, 3, State.Black);
 
             // WHITE SIDE
+            SetInitialLocation(3, 0, State.White);
             SetInitialLocation(3, 1, State.White);
             SetInitialLocation(3, 2, State.White);
-            SetInitialLocation(3, 3, State.White);
             SetInitialLocation(4, 0, State.White);
+            SetInitialLocation(4, 1, State.White);
             SetInitialLocation(4, 2, State.White);
-            SetInitialLocation(4, 4, State.White);
 
             // BLOCKED
-            SetInitialLocation(1, 0, State.Blocked);
-            SetInitialLocation(1, 4, State.Blocked);
             SetInitialLocation(2, 0, State.Blocked);
-            SetInitialLocation(2, 1, State.Blocked);
-            SetInitialLocation(2, 3, State.Blocked);
-            SetInitialLocation(2, 4, State.Blocked);
-            SetInitialLocation(3, 0, State.Blocked);
-            SetInitialLocation(3, 4, State.Blocked);
+            SetInitialLocation(2, 2, State.Blocked);
         }
     }
 }
